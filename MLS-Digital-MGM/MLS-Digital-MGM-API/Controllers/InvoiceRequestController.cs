acbf9b90f0410c76fd5e7b71394c200691ff892f
arthur <arthurkawalewale@gmail.com>
2024-12-05 17:08:03 +0200

    added function to show processed invoices

--- MLS-Digital-MGM/MLS-Digital-MGM-API/Controllers/InvoiceRequestController.cs
+++ MLS-Digital-MGM/MLS-Digital-MGM-API/Controllers/InvoiceRequestController.cs
@@ -128,6 +128,76 @@ namespace MLS_Digital_MGM_API.Controllers
             }
         }
 
+        [HttpGet("processedNotQuickBooks")]
+        public async Task<IActionResult> GetInvoiceRequestsNonQuickBooks(int pageNumber = 1, int pageSize = 10)
+        {
+            try
+            {
+                var dataTableParams = new DataTablesParameters();
+                string username = _httpContextAccessor.HttpContext.User.Identity.Name;
+                var user = await _repositoryManager.UserRepository.FindByEmailAsync(username);
+                string CreatedById = user.Id;
+
+                var pagingParameters = new PagingParameters<InvoiceRequest>
+                {
+                    Predicate = u => u.Status != Lambda.Paid,
+                    PageNumber = dataTableParams.LoadFromRequest(_httpContextAccessor) ? dataTableParams.PageNumber : pageNumber,
+                    PageSize = dataTableParams.LoadFromRequest(_httpContextAccessor) ? dataTableParams.PageSize : pageSize,
+                    SearchTerm = dataTableParams.LoadFromRequest(_httpContextAccessor) ? dataTableParams.SearchValue : null,
+                    SortColumn = dataTableParams.LoadFromRequest(_httpContextAccessor) ? dataTableParams.SortColumn : null,
+                    SortDirection = dataTableParams.LoadFromRequest(_httpContextAccessor) ? dataTableParams.SortColumnAscDesc : null,
+                    Includes = new Expression<Func<InvoiceRequest, object>>[] {
+                        p => p.CreatedBy,
+                        p => p.Customer,
+                        p => p.QBInvoice,
+                    },
+                };
+
+                var invoiceRequestsPaged = await _repositoryManager.InvoiceRequestRepository.GetPagedAsync(pagingParameters);
+
+                if (invoiceRequestsPaged == null || !invoiceRequestsPaged.Any())
+                {
+                    if (dataTableParams.LoadFromRequest(_httpContextAccessor))
+                    {
+                        var draw = dataTableParams.Draw;
+                        return Json(new
+                        {
+                            draw,
+                            recordsFiltered = 0,
+                            recordsTotal = 0,
+                            data = Enumerable.Empty<ReadInvoiceRequestDTO>()
+                        });
+                    }
+                    return Ok(Enumerable.Empty<ReadInvoiceRequestDTO>());
+                }
+
+                var invoiceRequestDTOs = _mapper.Map<List<ReadInvoiceRequestDTO>>(invoiceRequestsPaged.OrderByDescending(x => x.CreatedDate).ToList());
+
+                if (dataTableParams.LoadFromRequest(_httpContextAccessor))
+                {
+                    var draw = dataTableParams.Draw;
+                    var resultTotalFiltered = invoiceRequestDTOs.Count;
+                    var totalRecords = await _repositoryManager.InvoiceRequestRepository.CountAsync(pagingParameters);
+
+                    return Json(new
+                    {
+                        draw,
+                        recordsFiltered = totalRecords,
+                        recordsTotal = totalRecords,
+                        data = invoiceRequestDTOs.ToList()
+                    });
+                }
+
+                return Ok(invoiceRequestDTOs);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred in GetInvoiceRequests");
+                await _errorLogService.LogErrorAsync(ex);
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
         [HttpGet("processed")]
         public async Task<IActionResult> GetProcessedInvoiceRequests(int pageNumber = 1, int pageSize = 10)
         {
@@ -242,6 +312,8 @@ namespace MLS_Digital_MGM_API.Controllers
                 return StatusCode(500, "Internal server error");
             }
         }
+
+
 
         [HttpGet("cpdtrainings")]
         public async Task<IActionResult> GetCPDTrainings(int cpdTrainingId, int pageNumber = 1, int pageSize = 10)
